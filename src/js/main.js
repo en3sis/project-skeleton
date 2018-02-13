0a2b8e9aa2b7ae688c7bcd5b1c5447d1cd514239
Sorin Curescu <en3sis@gmail.com>
2018-02-13 18:17:32 +0100

    added jQuery, just cuz.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,1 @@
-function myFirstFunction(e){
-//do some action.
-window.addEventListener('keydown' , function(e){
-//do some action});
-
-function mySecondFunction(e){
-//do some action.
-window.addEventListener('keydown' , function(e){
-//do some action});
-
-//calling first function
-myFirstFunction();
-//add another event listener to make me go to the second function.
-window.addEventListener('keydown' , function(e){
-mySecondFunction();
-});
+$('#hello').html(`<h1> Hello world </h1>`)
